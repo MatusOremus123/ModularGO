15a45798ded7ee6a9c5fbf2db9e7ab6cb53d16bf
MatusOremus123 <matus.oremus@gmail.com>
2025-01-21 15:50:57 +0100

    New Cart Page

---
+++ lib/screens/cart_page.dart
@@ -0,0 +1,364 @@
+import 'package:flutter/material.dart';
+import 'shop_page.dart';
+import 'product_page.dart';
+import 'vending_machines_page.dart';
+import 'settings_page.dart';
+
+class CartPage extends StatelessWidget {
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      appBar: AppBar(
+        backgroundColor: Colors.yellow,
+        elevation: 0,
+        automaticallyImplyLeading: false,
+        title: Column(
+          crossAxisAlignment: CrossAxisAlignment.start,
+          children: [
+
+            Row(
+              crossAxisAlignment: CrossAxisAlignment.center,
+              children: [
+                // Image for modular PNG
+                Image.asset(
+                  'assets/MODULAR.png',
+                  height: 30, // Adjust height as needed
+                ),
+                SizedBox(width: 10),
+                // Text "SRH New Campus"
+                Text(
+                  'SRH New Campus',
+                  style: TextStyle(fontSize: 14, color: Colors.black),
+                ),
+              ],
+            ),
+            // Text "001"
+            Text(
+              '001',
+              style: TextStyle(
+                fontSize: 20,
+                fontWeight: FontWeight.bold,
+                color: Colors.black,
+              ),
+            ),
+            // Text "Development Lab"
+            Text(
+              'Development Lab',
+              style: TextStyle(fontSize: 16, color: Colors.black),
+            ),
+            // "Switch machines" link
+            GestureDetector(
+              onTap: () {
+                // Handle "Switch machines" tap
+              },
+              child: Text(
+                'Switch machines >',
+                style: TextStyle(
+                  fontSize: 14,
+                  color: Colors.blue,
+                  decoration: TextDecoration.underline,
+                ),
+              ),
+            ),
+          ],
+        ),
+        actions: [
+          Padding(
+            padding: const EdgeInsets.only(right: 16.0),
+            child: Icon(
+              Icons.storage,
+              size: 60,
+              color: Colors.grey,
+            ),
+          ),
+        ],
+        toolbarHeight: 160,
+      ),
+      body: Column(
+        crossAxisAlignment: CrossAxisAlignment.start,
+        children: [
+          Padding(
+            padding: const EdgeInsets.all(16.0),
+            child: Row(
+              mainAxisAlignment: MainAxisAlignment.spaceBetween,
+              children: [
+                Column(
+                  crossAxisAlignment: CrossAxisAlignment.start,
+                  children: [
+                    // Removed the text from here
+                  ],
+                ),
+              ],
+            ),
+          ),
+          Padding(
+            padding: const EdgeInsets.all(16.0),
+            child: TextField(
+              decoration: InputDecoration(
+                hintText: 'Search your shopping cart...',
+                prefixIcon: Icon(Icons.search),
+                border: OutlineInputBorder(
+                  borderRadius: BorderRadius.circular(10.0),
+                ),
+              ),
+            ),
+          ),
+          Padding(
+            padding: const EdgeInsets.symmetric(horizontal: 16.0),
+            child: Row(
+              mainAxisAlignment: MainAxisAlignment.spaceBetween,
+              children: [
+                ElevatedButton(
+                  onPressed: () {
+                    // Handle "This machine" button press
+                  },
+                  child: Text('This machine'),
+                  style: ElevatedButton.styleFrom(
+                    backgroundColor: Colors.black,
+                    foregroundColor: Colors.white,
+                  ),
+                ),
+                OutlinedButton(
+                  onPressed: () {
+                    // Handle "Other machines" button press
+                  },
+                  child: Text('Other machines'),
+                ),
+              ],
+            ),
+          ),
+          Padding(
+            padding: const EdgeInsets.all(16.0),
+            child: Column(
+              crossAxisAlignment: CrossAxisAlignment.start,
+              children: [
+                Text(
+                  'Cart in 001 (Development Lab)',
+                  style: TextStyle(
+                    fontSize: 18,
+                    fontWeight: FontWeight.bold,
+                  ),
+                ),
+                SizedBox(height: 16),
+                Card(
+                  elevation: 2,
+                  child: Padding(
+                    padding: const EdgeInsets.all(8.0),
+                    child: Row(
+                      children: [
+                        Image.asset(
+                          'assets/SketchPen.png',
+                          height: 50,
+                          width: 50,
+                          errorBuilder: (context, error, stackTrace) {
+                            return Icon(Icons.image_not_supported);
+                          },
+                        ),
+                        SizedBox(width: 10),
+                        Expanded(
+                          child: Column(
+                            crossAxisAlignment: CrossAxisAlignment.start,
+                            children: [
+                              Text(
+                                'Sketch Pen',
+                                style: TextStyle(
+                                  fontSize: 16,
+                                  fontWeight: FontWeight.bold,
+                                ),
+                              ),
+                              Text(
+                                'Sakura\'s Micron Pigma pen, blau',
+                                style: TextStyle(fontSize: 14),
+                              ),
+                              GestureDetector(
+                                onTap: () {
+                                  // Handle "Details" tap
+                                },
+                                child: Text(
+                                  'Details',
+                                  style: TextStyle(
+                                    fontSize: 14,
+                                    color: Colors.blue,
+                                    decoration: TextDecoration.underline,
+                                  ),
+                                ),
+                              ),
+                            ],
+                          ),
+                        ),
+                        Column(
+                          children: [
+                            Row(
+                              children: [
+                                IconButton(
+                                  onPressed: () {
+                                    // Handle decrease quantity
+                                  },
+                                  icon: Icon(Icons.remove),
+                                ),
+                                Text('1'),
+                                IconButton(
+                                  onPressed: () {
+                                    // Handle increase quantity
+                                  },
+                                  icon: Icon(Icons.add),
+                                ),
+                              ],
+                            ),
+                            Row(
+                              children: [
+                                IconButton(
+                                  onPressed: () {
+                                    // Handle favorite action
+                                  },
+                                  icon: Icon(Icons.favorite_border),
+                                ),
+                                IconButton(
+                                  onPressed: () {
+                                    // Handle delete action
+                                  },
+                                  icon: Icon(Icons.delete_outline),
+                                ),
+                              ],
+                            ),
+                          ],
+                        ),
+                        SizedBox(width: 10),
+                        Text(
+                          '3,30 €',
+                          style: TextStyle(
+                            fontSize: 16,
+                            fontWeight: FontWeight.bold,
+                          ),
+                        ),
+                      ],
+                    ),
+                  ),
+                ),
+              ],
+            ),
+          ),
+        ],
+      ),
+      bottomNavigationBar: Column(
+        mainAxisSize: MainAxisSize.min,
+        children: [
+          // Total amount container
+          Container(
+            height: 80,
+            padding: const EdgeInsets.symmetric(horizontal: 16.0),
+            child: Row(
+              mainAxisAlignment: MainAxisAlignment.spaceBetween,
+              children: [
+                Text(
+                  'Total: 3,30 €',
+                  style: TextStyle(
+                    fontSize: 18,
+                    fontWeight: FontWeight.bold,
+                  ),
+                ),
+                ElevatedButton(
+                  onPressed: () {
+                    // Handle Checkout
+                  },
+                  child: Text('Check Out'),
+                  style: ElevatedButton.styleFrom(
+                    backgroundColor: Colors.black,
+                    foregroundColor: Colors.white,
+                  ),
+                ),
+              ],
+            ),
+          ),
+          Divider(height: 0, thickness: 1),
+          Container(
+            height: 80, // Increased height for the task bar
+            padding: const EdgeInsets.symmetric(horizontal: 16.0),
+            child: Row(
+              mainAxisAlignment: MainAxisAlignment.spaceBetween,
+              children: [
+                _buildTaskBarItem(
+                  icon: Icons.home,
+                  label: "Home",
+                  onTap: () {
+                    // Navigate to ShopPage
+                    Navigator.pushReplacement(
+                      context,
+                      MaterialPageRoute(builder: (context) => ShopPage()),
+                    );
+                  },
+                ),
+                _buildTaskBarItem(
+                  icon: Icons.edit,
+                  label: "Products",
+                  onTap: () {
+                    // Navigate to ProductPage
+                    Navigator.pushReplacement(
+                      context,
+                      MaterialPageRoute(builder: (context) => ProductPage()),
+                    );
+                  },
+                ),
+                _buildTaskBarItem(
+                  icon: Icons.build,
+                  label: "Machines",
+                  onTap: () {
+                    // Navigate to VendingMachinesPage
+                    Navigator.pushReplacement(
+                      context,
+                      MaterialPageRoute(
+                          builder: (context) => VendingMachinesPage()),
+                    );
+                  },
+                ),
+                _buildTaskBarItem(
+                  icon: Icons.shopping_cart,
+                  label: "Cart",
+                  onTap: () {
+                    // Current page
+                  },
+                ),
+                _buildTaskBarItem(
+                  icon: Icons.settings,
+                  label: "Settings",
+                  onTap: () {
+                    // Navigate to SettingsPage
+                    Navigator.pushReplacement(
+                      context,
+                      MaterialPageRoute(builder: (context) => SettingsPage()),
+                    );
+                  },
+                ),
+              ],
+            ),
+          ),
+        ],
+      ),
+    );
+  }
+
+  Widget _buildTaskBarItem({
+    required IconData icon,
+    required String label,
+    required VoidCallback onTap,
+  }) {
+    return GestureDetector(
+      onTap: onTap,
+      child: Column(
+        mainAxisAlignment: MainAxisAlignment.center,
+        children: [
+          Icon(icon, color: Colors.grey),
+          SizedBox(height: 4),
+          Text(
+            label,
+            style: TextStyle(
+              fontFamily: 'Roboto',
+              color: Colors.grey,
+              fontSize: 12,
+            ),
+          ),
+        ],
+      ),
+    );
+  }
+}
